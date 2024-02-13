853db19f4b5a8037ac1ffab432422a34530662be
Yavor Gornalov <y.gornalov@gmail.com>
2024-02-13 20:56:26 +0200

    Data-Types Task08 and Task11 --uploaded

---
+++ S05_Data-Types-Exercise/P08_Beer-Kegs/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+internal class Program
+{
+    public class BeerKeg
+    {
+        public string Model { get; set; }
+        public double Radius { get; set; }
+        public int Height { get; set; }
+
+        public BeerKeg(string model, double radius, int height)
+        {
+            Model = model;
+            Radius = radius;
+            Height = height;
+        }
+
+        public double GetVolume()
+        {
+            return Math.PI * Math.Pow(Radius, 2) * Height;
+        }
+    }
+
+    static void Main()
+    {
+        List<BeerKeg> kegs = new List<BeerKeg>();
+        int num = int.Parse(Console.ReadLine());
+        for (int i = 0; i < num; i++)
+        {
+            BeerKeg newKeg = new BeerKeg
+                (
+                    model: Console.ReadLine(),
+                    radius: double.Parse(Console.ReadLine()),
+                    height: int.Parse(Console.ReadLine())
+                );
+            kegs.Add(newKeg);
+        }
+
+        BeerKeg biggestKeg = kegs.OrderByDescending(bk => bk.GetVolume()).FirstOrDefault();
+        Console.WriteLine(biggestKeg.Model);
+    }
+}
---
+++ S05_Data-Types-Exercise/P11_Snowballs/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int maxSnowballQuality = 0, maxSnowballWeight = 0, maxSnowballTime = 0;
+        BigInteger maxSnowballValue = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            int snowballWeight = int.Parse(Console.ReadLine());
+            int snowballTime = int.Parse(Console.ReadLine());
+            int snowballQuality = int.Parse(Console.ReadLine());
+
+            BigInteger snowballValue = BigInteger.Pow(snowballWeight / snowballTime, snowballQuality);
+
+            if (snowballValue > maxSnowballValue)
+            {
+                maxSnowballValue = snowballValue;
+                maxSnowballWeight = snowballWeight;
+                maxSnowballTime = snowballTime;
+                maxSnowballQuality = snowballQuality;
+            }
+        }
+
+        Console.WriteLine($"{maxSnowballWeight} : {maxSnowballTime} = {maxSnowballValue} ({maxSnowballQuality})");
+    }
+}
