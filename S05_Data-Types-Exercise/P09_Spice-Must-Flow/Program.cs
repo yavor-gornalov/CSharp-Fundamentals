9c81087a51779cb79e0ac7074ed32868212c5db6
Yavor Gornalov <y.gornalov@gmail.com>
2024-02-14 18:46:37 +0200

    Data-Types Exercise --finished

---
+++ S05_Data-Types-Exercise/P09_Spice-Must-Flow/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int DAILY_REDUCE = 10, DAILY_CONSUMPTION = 26;
+        int yieldMined = 0, miningDays = 0;
+
+        int yieldAvailable = int.Parse(Console.ReadLine());
+
+        while (yieldAvailable >= 100)
+        {
+            yieldMined += yieldAvailable;
+            yieldAvailable -= DAILY_REDUCE;
+            yieldMined -= DAILY_CONSUMPTION;
+            miningDays += 1;
+        }
+        yieldMined = Math.Max(yieldMined - DAILY_CONSUMPTION, 0);
+        Console.WriteLine(miningDays);
+        Console.WriteLine(yieldMined);
+    }
+}
---
+++ S05_Data-Types-Exercise/P10_Poke-Mon/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace P10_Poke_Mon
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int initialPokemonPower = int.Parse(Console.ReadLine());
+            int distanceToTarget = int.Parse(Console.ReadLine());
+            int exhaustionFactor = int.Parse(Console.ReadLine());
+
+            int targetsCounter = 0, pokemonPower = initialPokemonPower;
+            while (pokemonPower >= distanceToTarget)
+            {
+                if (pokemonPower == initialPokemonPower / 2.0)
+                {
+                    if (exhaustionFactor > 1)
+                    {
+                        pokemonPower /= exhaustionFactor;
+                        continue;
+                    }
+                }
+                targetsCounter += 1;
+                pokemonPower -= distanceToTarget;
+
+            }
+            Console.WriteLine(pokemonPower);
+            Console.WriteLine(targetsCounter);
+        }
+    }
+}
