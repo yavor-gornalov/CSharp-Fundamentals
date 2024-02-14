dbfaee4cc2f82504e036cb62f6fe7ad1e5641bac
Yavor Gornalov <y.gornalov@gmail.com>
2024-02-14 20:12:56 +0200

    Data-Types More exercises --finished

---
+++ S06_Data-Types-More/P06_Balanced-Brackets/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        string isBalanced = "BALANCED";
+        char target = '(';
+
+        for (int i = 0; i < n; i++)
+        {
+            char character;
+            Char.TryParse(Console.ReadLine(), out character);
+            if (character == '(' || character == ')')
+            {
+                if (character != target || n <= 1)
+                {
+                    isBalanced = "UNBALANCED";
+                    break;
+                }
+                target = target == '(' ? ')' : '(';
+            }
+        }
+        if (target != ')')
+            Console.WriteLine(isBalanced);
+        else Console.WriteLine("UNBALANCED");
+    }
+}
