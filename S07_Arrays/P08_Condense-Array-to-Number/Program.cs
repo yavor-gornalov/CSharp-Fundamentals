9643a17dbe23e682ceee5c3a517503a4cfe24f48
Yavor Gornalov <y.gornalov@gmail.com>
2024-02-16 09:08:37 +0200

    Task08 --uploaded, Arrays-Lab --finished

---
+++ S07_Arrays/P08_Condense-Array-to-Number/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        List<int> nums = Console.ReadLine()
+                                .Split()
+                                .Select(int.Parse)
+                                .ToList();
+
+        while (nums.Count > 1)
+        {
+            List<int> condensed = new();
+            for (int i = 1; i < nums.Count; i++)
+            {
+                condensed.Add(nums[i - 1] + nums[i]);
+            }
+            nums = condensed;
+        }
+        Console.WriteLine(nums[0]);
+    }
+}
