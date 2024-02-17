05e62a98ac73ab96cac3c7e6221b60d186a7b825
Yavor Gornalov <y.gornalov@gmail.com>
2024-02-17 14:11:24 +0200

    08.Array, Task09,10 --uploaded

---
+++ S08_Arrays-Exercise/P09_Kamino-Factory/Program.cs
@@ -0,0 +1,69 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        int seqLength = int.Parse(Console.ReadLine());
+
+        int bestIndex = -1;
+        int bestLength = -1;
+        int bestSampleNumber = -1;
+        int bestSum = 0;
+        int[] bestSequence = null;
+
+        int sampleNumber = 0;
+        while (true)
+        {
+            sampleNumber++;
+            string line = Console.ReadLine();
+
+            if (line == "Clone them!") break;
+
+            int[] sequence = line.Split("!", StringSplitOptions.RemoveEmptyEntries)
+                                  .Select(int.Parse)
+                                  .ToArray();
+
+
+
+            (int index, int length) = getMaxSequence(sequence);
+
+            if ((length > bestLength)
+                || (length == bestLength) && (index < bestIndex)
+                || (length == bestLength) && (index == bestIndex) && (sequence.Sum() > bestSum))
+            {
+                bestSampleNumber = sampleNumber;
+                bestSequence = sequence;
+                bestLength = length;
+                bestIndex = index;
+                bestSum = sequence.Sum();
+            }
+        }
+        Console.WriteLine($"Best DNA sample {bestSampleNumber} with sum: {bestSum}.");
+        Console.WriteLine(string.Join(" ", bestSequence));
+    }
+
+    private static Tuple<int, int> getMaxSequence(int[] arr)
+    {
+        int maxIndex = -1;
+        int maxCounter = -1;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            int currentIndex = i;
+            int currentCounter = 1;
+            for (int j = i + 1; j < arr.Length; j++)
+            {
+                int currentNumber = arr[i];
+                int nextNumber = arr[j];
+
+                if (currentNumber != nextNumber) break;
+
+                currentCounter++;
+            }
+            if (currentCounter > maxCounter)
+            {
+                maxCounter = currentCounter;
+                maxIndex = currentIndex;
+            }
+        }
+        return Tuple.Create(maxIndex, maxCounter);
+    }
+}
---
+++ S08_Arrays-Exercise/P10_LadyBugs/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+internal class Program
+{
+    static void Main()
+    {
+        int fieldSize = int.Parse(Console.ReadLine());
+        if (fieldSize <= 0) return;
+
+        int[] bugIndexes = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+        int[] field = new int[fieldSize];
+        foreach (int bugIndex in bugIndexes)
+        {
+            if (bugIndex < 0 || bugIndex >= field.Length) continue;
+            field[bugIndex] = 1;
+        }
+
+        while (true)
+        {
+            string line = Console.ReadLine();
+
+            if (line == "end") break;
+
+            string[] tokens = line.Split().ToArray();
+
+            int index = int.Parse(tokens[0]);
+            string direction = tokens[1];
+            int step = int.Parse(tokens[2]);
+
+            if (index < 0 || index >= field.Length || field[index] == 0) continue;
+            field[index] = 0;
+
+            if (step < 0)
+            {
+                direction = direction == "right" ? "left" : "right";
+                step = -step;
+            }
+
+            if (direction == "right")
+            {
+                int nextIndex = index + step;
+
+                if (nextIndex >= field.Length) continue;
+
+                for (int i = nextIndex; i < field.Length; i += step)
+                {
+                    if (field[i] == 1) continue;
+                    field[i] = 1;
+                    break;
+                }
+            }
+            if (direction == "left")
+            {
+                int nextIndex = index - step;
+
+                if (nextIndex < 0) continue;
+
+                for (int i = nextIndex; i >= 0; i -= step)
+                {
+                    if (field[i] == 1) continue;
+                    field[i] = 1;
+                    break;
+                }
+            }
+        }
+        Console.WriteLine(string.Join(" ", field));
+    }
+}
