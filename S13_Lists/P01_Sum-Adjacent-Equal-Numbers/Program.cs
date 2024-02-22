83594f82225aefadf74bc196e44ea6fac94f235b
Yavor Gornalov <ygor85@students.softuni.bg>
2024-02-22 22:48:01 +0200

    List-Lab --started

---
+++ S13_Lists/P01_Sum-Adjacent-Equal-Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        List<double> nums = Console.ReadLine()
+                                   .Split()
+                                   .Select(double.Parse)
+                                   .ToList();
+
+        for (int i = 0; i < nums.Count - 1; i++)
+        {
+            if (nums[i] == nums[i + 1])
+            {
+                nums[i] += nums[i + 1];
+                nums.RemoveAt(i + 1);
+                i = -1;
+            }
+        }
+
+        Console.WriteLine(string.Join(" ", nums));
+    }
+}
---
+++ S13_Lists/P02_Gauss-Trick/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+        List<int> result = new List<int>();
+        for (int i = 0; i < nums.Count / 2; i++)
+        {
+            result.Add(nums[i] + nums[nums.Count - i - 1]);
+        }
+        if (nums.Count % 2 != 0)
+        {
+            result.Add(nums[nums.Count / 2]);
+        }
+        Console.WriteLine(string.Join(" ", result));
+    }
+}
