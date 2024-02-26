f8e440793f545fb24e3f656815242bb1e39ac85d
Yavor Gornalov <y.gornalov@gmail.com>
2024-02-26 19:47:53 +0200

    Lists, Homework --finished

---
+++ S14_Lists-Exercise/P06_Cards-Game/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+
+internal class Program
+{
+    static void Main()
+    {
+        List<int> firstPlayerCards = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToList();
+        List<int> secondPlayerCards = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToList();
+
+        string winner = string.Empty;
+        while (true)
+        {
+            if (firstPlayerCards.Count == 0)
+            {
+                winner = "Second";
+                break;
+            }
+
+            if (secondPlayerCards.Count == 0)
+            {
+                winner = "First";
+                break;
+            }
+            int firstCard = firstPlayerCards.First();
+            firstPlayerCards.Remove(firstCard);
+            int secondCard = secondPlayerCards.First();
+            secondPlayerCards.Remove(secondCard);
+            int currentHand = firstCard + secondCard;
+
+            if (firstCard > secondCard)
+            {
+                firstPlayerCards.Add(secondCard);
+                firstPlayerCards.Add(firstCard);
+            }
+            else if (firstCard < secondCard)
+            {
+                secondPlayerCards.Add(firstCard);
+                secondPlayerCards.Add(secondCard);
+            }
+        }
+
+        Console.WriteLine($"{winner} player wins! Sum: {Math.Abs(firstPlayerCards.Sum() - secondPlayerCards.Sum())}");
+    }
+}
---
+++ S14_Lists-Exercise/P07_Append-Arrays/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        List<string> numbersStr = Console.ReadLine()
+            .Split("|")
+            .ToList();
+        numbersStr.Reverse();
+
+        List<int> numbers = new List<int>();
+        foreach (string part in numbersStr)
+        {
+            List<int> currentList = part
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            numbers.AddRange(currentList);
+        }
+        Console.WriteLine(string.Join(" ", numbers));
+    }
+}
