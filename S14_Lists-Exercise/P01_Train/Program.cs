dc1348c8ee19618dda094140f5d2970ed3ba9c25
Yavor Gornalov <y.gornalov@gmail.com>
2024-02-26 11:55:47 +0200

    List-Exercises --started

---
+++ S14_Lists-Exercise/P01_Train/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        List<int> train = Console.ReadLine().Split().Select(int.Parse).ToList();
+        int maxCapacity = int.Parse(Console.ReadLine());
+
+
+        while (true)
+        {
+            string line = Console.ReadLine();
+
+            if (line == "end")
+            {
+                Console.WriteLine(string.Join(" ", train));
+                break;
+            }
+            else if (line.StartsWith("Add"))
+            {
+                int newWagon = int.Parse(line.Split()[1]);
+                train.Add(newWagon);
+            }
+            else
+            {
+                int passedgers = int.Parse(line);
+                for (int i = 0; i < train.Count; i++)
+                {
+                    if (train[i] + passedgers <= maxCapacity)
+                    {
+                        train[i] += passedgers;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
