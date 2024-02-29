f6444019731fcfc06dd269131f74a03eeeaf3b02
Yavor Gornalov <y.gornalov@gmail.com>
2024-02-29 12:18:13 +0200

    Lists-More exercises --finished

---
+++ S15_Lists-More/P05_Drum-Set/Program.cs
@@ -0,0 +1,45 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        double savings = double.Parse(Console.ReadLine());
+        List<int> initialBrumQualities = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToList();
+        List<int> currentDrumQualities = new List<int>();
+        currentDrumQualities.AddRange(initialBrumQualities);
+
+        while (true)
+        {
+            string line = Console.ReadLine();
+
+            if (line == "Hit it again, Gabsy!") break;
+
+            if (currentDrumQualities.Count == 0) break;
+
+            int hitPower = int.Parse(line);
+
+            for (int i = 0; i < currentDrumQualities.Count; i++)
+            {
+                if (currentDrumQualities[i] <= hitPower)
+                {
+                    int drumCost = initialBrumQualities[i] * 3;
+                    if (drumCost > savings)
+                    {
+                        currentDrumQualities.RemoveAt(i);
+                        initialBrumQualities.RemoveAt(i);
+                        i--;
+                        continue;
+                    }
+                    currentDrumQualities[i] = initialBrumQualities[i];
+                    savings -= drumCost;
+                    continue;
+                }
+                currentDrumQualities[i] -= hitPower;
+            }
+        }
+        Console.WriteLine(string.Join(" ", currentDrumQualities));
+        Console.WriteLine($"Gabsy has {savings:F2}lv.");
+    }
+}
