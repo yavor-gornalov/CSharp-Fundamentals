5e6670166a1fd03a4a0af24bb879558c0cc120ae
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-01 20:03:13 +0200

    Object-and-Classes-Exercises --started

---
+++ S17_Objects-and-Classes-Exercise/P01_Advertisement-Message/Program.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        Advertisement adv = new Advertisement();
+
+        for (int i = 0; i < n; i++)
+        {
+            adv.PrintRandomAdvertisement();
+        }
+    }
+}
+
+public class Advertisement
+{
+    public List<string> Phrases { get; set; }
+    public List<string> Events { get; set; }
+    public List<string> Authors { get; set; }
+
+    public List<string> Cities { get; set; }
+
+    public Advertisement()
+    {
+        Phrases = new List<string>() { "Excellent product.", "Such a great product.", "I always use that product.", "Best product of its category.", "Exceptional product.", "I can't live without this product." };
+        Events = new List<string>() { "Now I feel good.", "I have succeeded with this product.", "Makes miracles. I am happy of the results!", "I cannot believe but now I feel awesome.", "Try it yourself, I am very satisfied.", "I feel great!" };
+        Authors = new List<string>() { "Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva" };
+        Cities = new List<string>() { "Burgas", "Sofia", "Plovdiv", "Varna", "Ruse" };
+    }
+    public void PrintRandomAdvertisement()
+    {
+        Random random = new Random();
+        string phrase = Phrases[random.Next(0, Phrases.Count)];
+        string evnt = Events[random.Next(0, Events.Count)];
+        string author = Authors[random.Next(0, Authors.Count)];
+        string city = Cities[random.Next(0, Cities.Count)];
+
+        Console.WriteLine($"{phrase} {evnt} {author} – {city}.");
+    }
+}
---
+++ S17_Objects-and-Classes-Exercise/P02_Articles/Program.cs
@@ -0,0 +1,78 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        string[] args = Console.ReadLine().Split(", ").ToArray();
+
+        string title = args[0];
+        string content = args[1];
+        string author = args[2];
+
+        Article newArticle = new Article(title, content, author);
+
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] tokens = Console.ReadLine().Split(": ").ToArray();
+
+            string command = tokens[0];
+            string argument = tokens[1];
+
+            switch (command)
+            {
+                case "Edit":
+                    {
+                        newArticle.Edit(argument);
+                        break;
+                    }
+                case "ChangeAuthor":
+                    {
+                        newArticle.CangeAuthor(argument);
+                        break;
+                    }
+                case "Rename":
+                    {
+                        newArticle.Rename(argument);
+                        break;
+                    }
+            }
+        }
+        Console.WriteLine(newArticle.ToString());
+    }
+}
+
+class Article
+{
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public string Author { get; set; }
+
+    public Article(string title, string content, string author)
+    {
+        Title = title;
+        Content = content;
+        Author = author;
+    }
+
+    public override string ToString()
+    {
+        return $"{Title} - {Content}: {Author}";
+    }
+
+    public void Edit(string newContent)
+    {
+        Content = newContent;
+    }
+
+    public void CangeAuthor(string newAuthor)
+    {
+        Author = newAuthor;
+    }
+
+    public void Rename(string newTitle)
+    {
+        Title = newTitle;
+    }
+
+}
