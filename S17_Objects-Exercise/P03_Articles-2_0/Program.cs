c115e65b78c27780e803efda35ffb4a851398374
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-01 22:23:46 +0200

    Objects Task01-04 --uploaded

---
+++ S17_Objects-Exercise/P03_Articles-2_0/Program.cs
@@ -0,0 +1,62 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        Newsletter newsletter = new Newsletter();
+
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            string[] args = Console.ReadLine().Split(", ").ToArray();
+
+            string title = args[0];
+            string content = args[1];
+            string author = args[2];
+
+            Article newArticle = new Article(title, content, author);
+            newsletter.AddArticle(newArticle);
+        }
+        Console.WriteLine(newsletter.ToString());
+    }
+}
+
+class Newsletter
+{
+    public List<Article> Articles { get; set; }
+
+    public Newsletter()
+    {
+        Articles = new List<Article>();
+    }
+
+    public void AddArticle(Article article) => Articles.Add(article);
+
+    public override string ToString()
+    {
+        string result = "";
+        foreach (Article article in Articles)
+        {
+            Console.WriteLine(article.ToString());
+        }
+        return result;
+    }
+}
+
+class Article
+{
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public string Author { get; set; }
+
+    public Article(string title, string content, string author)
+    {
+        Title = title;
+        Content = content;
+        Author = author;
+    }
+
+    public override string ToString()
+    {
+        return $"{Title} - {Content}: {Author}";
+    }
+}
---
+++ S17_Objects-Exercise/P04_Students/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+internal class Program
+{
+    static void Main()
+    {
+        List<Student> students = new List<Student>();
+
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] tokens = Console.ReadLine().Split().ToArray();
+            string firstName = tokens[0];
+            string lastName = tokens[1];
+            double grade = double.Parse(tokens[2]);
+
+            Student newStudent = new Student(firstName, lastName, grade);
+
+            students.Add(newStudent);
+        }
+
+        foreach (Student student in students.OrderBy(x => -x.Grade))
+        {
+            Console.WriteLine(student.ToString());
+        }
+    }
+}
+
+public class Student
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public double Grade { get; set; }
+
+    public Student(string firstName, string lastName, double grade)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Grade = grade;
+    }
+
+    public override string ToString()
+    {
+        return $"{FirstName} {LastName}: {Grade:F2}";
+    }
+}
