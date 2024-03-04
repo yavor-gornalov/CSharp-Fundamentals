00907d72fab3ee8981ec170060bd218c1fa59e64
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-04 08:09:43 +0200

    Occurrences --uploaded

---
+++ S19_Associative-Arrays/P02_Odd-Occurrences/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        Dictionary<string, int> words = new Dictionary<string, int>();
+
+        foreach (string key in Console.ReadLine()
+            .Split()
+            .ToArray())
+        {
+            string word = key.ToLower();
+            if (!words.ContainsKey(word))
+            {
+                words.Add(word, 0);
+            }
+            words[word]++;
+        }
+
+        foreach (var pair in words)
+        {
+            if (pair.Value % 2 != 0)
+            {
+                Console.Write(pair.Key + " ");
+            }
+        }
+    }
+}
