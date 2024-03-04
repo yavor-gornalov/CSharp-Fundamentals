aa7045c0aef8690b375a7fab507e6bd904c6e37f
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-04 08:11:08 +0200

    Word-Filter --uploaded

---
+++ S19_Associative-Arrays/P04_Word-Filter/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        string[] words = Console.ReadLine().Split().ToArray();
+
+        foreach (var word in words)
+        {
+            Console.Write(word.Length % 2 == 0 ? $"{word}\n" : "");
+        }
+    }
+}
