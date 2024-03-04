baad05031d8e64c95f8a7c8d2258a2aa69d4adac
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-04 08:09:18 +0200

    Count-Real-Numbers --uploaded

---
+++ S19_Associative-Arrays/P01_Count-Real-Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        SortedDictionary<double, double> numbersCount = new SortedDictionary<double, double>();
+
+        foreach (double num in Console.ReadLine()
+            .Split()
+            .Select(double.Parse)
+            .ToArray())
+        {
+            if (!numbersCount.ContainsKey(num))
+            {
+                numbersCount.Add(num, 0);
+            }
+            numbersCount[num]++;
+        }
+
+        foreach (var pair in numbersCount)
+        {
+            Console.WriteLine($"{pair.Key} -> {pair.Value}");
+        }
+    }
+}
