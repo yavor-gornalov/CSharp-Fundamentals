0f9b79986d10580dd7dfdcedb470236e8218326a
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-04 08:10:36 +0200

    Word-Synonyms --uploaded

---
+++ S19_Associative-Arrays/P03_Word-Synonyms/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        Dictionary<string, List<string>> wordSynonims = new Dictionary<string, List<string>>();
+
+        int N = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < N; i++)
+        {
+            string word = Console.ReadLine();
+            string synonim = Console.ReadLine();
+
+            if (!wordSynonims.ContainsKey(word))
+            {
+                wordSynonims.Add(word, new List<string>());
+            }
+            wordSynonims[word].Add(synonim);
+        }
+
+        foreach (var pair in wordSynonims)
+        {
+            Console.WriteLine($"{pair.Key} - {string.Join(", ", pair.Value)}");
+        }
+    }
+}
