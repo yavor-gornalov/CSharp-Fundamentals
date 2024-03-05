89430855dcc4b23c67fa60861022089143a9ae95
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-05 11:40:17 +0200

    Miner-Task --uploaded

---
+++ S20_Associative-Arrays-Exercise/P02_Miner-Task/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        Dictionary<string, int> resources = new Dictionary<string, int>();
+        while (true)
+        {
+            string line = Console.ReadLine();
+
+            if (line == "stop")
+            {
+                foreach (var pair in resources) Console
+                        .WriteLine($"{pair.Key} -> {pair.Value}");
+                break;
+            }
+
+            string key = line;
+            int value = int.Parse(Console.ReadLine());
+
+            if (!resources.ContainsKey(key))
+            {
+                resources.Add(key, 0);
+            }
+
+            resources[key] += value;
+        }
+    }
+}
