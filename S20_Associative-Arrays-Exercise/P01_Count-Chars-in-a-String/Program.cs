259da08158025b7d589f9f7d02d16e190a73c076
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-05 11:39:58 +0200

    Count-Chars --uploaded

---
+++ S20_Associative-Arrays-Exercise/P01_Count-Chars-in-a-String/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        Dictionary<char, int> letterCount = new Dictionary<char, int>();
+
+        foreach (char letter in Console.ReadLine().ToCharArray())
+        {
+            if (letter == ' ') continue;
+
+            if (!letterCount.ContainsKey(letter))
+            {
+                letterCount.Add(letter, 0);
+            }
+            letterCount[letter]++;
+        }
+
+        foreach (var pair in letterCount)
+        {
+            Console.WriteLine($"{pair.Key} -> {pair.Value}");
+        }
+    }
+}
