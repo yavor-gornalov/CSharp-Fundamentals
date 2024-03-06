9f19e692cecb9d1bd5f6d74792e3490c9791259d
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-06 10:54:25 +0200

    Academy-Problem --uploaded

---
+++ S20_Associative-Arrays-Exercise/P06._Academy/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        Dictionary<string, List<double>> studentsGrades = new Dictionary<string, List<double>>();
+        int N = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < N; i++)
+        {
+            string student = Console.ReadLine();
+            double grade = double.Parse(Console.ReadLine());
+
+            if (!studentsGrades.ContainsKey(student))
+            {
+                studentsGrades.Add(student, new List<double>());
+            }
+            studentsGrades[student].Add(grade);
+        }
+
+        foreach (var pair in studentsGrades)
+        {
+            double avgGrade = pair.Value.Average();
+            if (avgGrade >= 4.5)
+            {
+                Console.WriteLine($"{pair.Key} -> {avgGrade:F2}");
+            }
+        }
+    }
+}
