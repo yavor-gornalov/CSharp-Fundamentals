558e2afc5c9ea652c3b01023d15909b2c6e0dbf3
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-07 09:47:50 +0200

    Reversed-Strings --uploaded

---
+++ S22_Text-Processing/P01_Reverse-Strings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+internal class Program
+{
+    static void Main()
+    {
+        StringBuilder output = new StringBuilder();
+        while (true) { 
+            string word = Console.ReadLine();
+
+            if ( word == "end")
+            {
+                Console.WriteLine(output);
+                break;
+            }
+            output.Append($"{word} = ");
+            char[] letters = word.ToCharArray();
+            foreach (char c in letters.Reverse())
+            {
+                output.Append(c);
+            }
+            output.Append('\n');
+        }
+    }
+}
