12f70ad5d332421241ca763bcc5e2b63d9a9d0b4
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-07 09:49:56 +0200

    Digit-Letters-and-Other --uploaded

---
+++ S22_Text-Processing/P05_Digits-Letters-and-Other/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        string line = Console.ReadLine();
+
+        string digits = string.Empty;
+        string letters = string.Empty;
+        string symbols = string.Empty;
+
+        foreach (char symbol in line.ToCharArray())
+        {
+            if (char.IsDigit(symbol))
+            {
+                digits += symbol - '0';
+            }
+            else if (char.IsLetter(symbol))
+            {
+                letters += symbol;
+            }
+            else
+            {
+                symbols += symbol;
+            }
+        }
+        Console.WriteLine(digits);
+        Console.WriteLine(letters);
+        Console.WriteLine(symbols);
+    }
+}
