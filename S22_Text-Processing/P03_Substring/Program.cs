95cbc8ef67c3bb35ac3b657623c2cb7a9cd27d8d
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-07 09:48:38 +0200

    Substring --uploaded

---
+++ S22_Text-Processing/P03_Substring/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        string substring = Console.ReadLine();
+        string mainString = Console.ReadLine();
+
+        while (mainString.Contains(substring))
+        {
+            mainString = mainString.Replace(substring, "");
+        }
+        Console.WriteLine(mainString);
+    }
+}
