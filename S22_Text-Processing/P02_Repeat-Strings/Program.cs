7cf4ec861b2b128d56d8d3c883660aaf4e6a3c07
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-07 09:48:07 +0200

    Repeat-Strings --uploaded

---
+++ S22_Text-Processing/P02_Repeat-Strings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+internal class Program
+{
+    static void Main()
+    {
+        StringBuilder output = new StringBuilder();
+        List<string> words = Console.ReadLine()
+            .Split()
+            .ToList();
+
+        foreach (var word in words)
+        {
+            int repetitions = word.Length;
+            foreach (string repeatedWord in Enumerable.Repeat(word, repetitions))
+            {
+                output.Append(repeatedWord);
+            }
+        }
+        Console.WriteLine(output);
+    }
+}
