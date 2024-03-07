b92096c6fdb6e511b0708005353e455630f40295
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-07 09:49:05 +0200

    Text-Filter --uploaded

---
+++ S22_Text-Processing/P04_Text-Filter/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        string[] bannedWords = Console.ReadLine()
+            .Split(", ")
+            .OrderByDescending(x => x.Length)
+            .ToArray();
+
+        string text = Console.ReadLine();
+
+        foreach (string word in bannedWords)
+        {
+            string target = new String('*', word.Length);
+
+            while (text.Contains(word))
+            {
+                text = text.Replace(word, target);
+            }
+
+        }
+        Console.WriteLine(text);
+    }
+}
