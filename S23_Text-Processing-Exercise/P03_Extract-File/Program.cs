eeac731537abf59e93efc57e7dbe4d69608a0665
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-08 15:09:59 +0200

    Extract-File --uploaded

---
+++ S23_Text-Processing-Exercise/P03_Extract-File/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        string line = Console.ReadLine();
+
+        string file = line.Split("\\").ToArray()[^1];
+
+        string[] tokens = file.Split(".").ToArray();
+        string fileName = tokens[0];
+        string fileExtension = tokens[1];
+
+        Console.WriteLine($"File name: {fileName}\nFile extension: {fileExtension}\n");
+    }
+}
