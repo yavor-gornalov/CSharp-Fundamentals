7277b7a76855d18394dd8658cf80cb8d77862461
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-08 15:08:57 +0200

    Valid-Usernames --uploaded

---
+++ S23_Text-Processing-Exercise/01_Valid-Usernames/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        char[] dashes = new char[] { '-', '_' };
+        string[] usernames = Console.ReadLine()
+            .Split(", ")
+            .ToArray();
+
+        foreach (string user in usernames)
+        {
+            if (user.Length < 3 || user.Length > 16) continue;
+
+            bool isValid = true;
+            foreach (char symbol in user.ToCharArray())
+            {
+                if (!char.IsLetterOrDigit(symbol) && !dashes.Contains(symbol))
+                {
+                    isValid = false;
+                    break;
+                }
+            }
+            if (isValid) Console.WriteLine(user);
+        }
+
+    }
+}
