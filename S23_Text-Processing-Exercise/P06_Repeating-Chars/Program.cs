24ad351db701a908e8fc8380ab852dfa3db9c406
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-09 16:22:22 +0200

    Repeating-Chars --uploaded

---
+++ S23_Text-Processing-Exercise/P06_Repeating-Chars/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        string text = Console.ReadLine();
+        string filteredText = text[0].ToString();
+        for (int i = 1; i < text.Length; i++)
+        {
+
+            if (text[i] != text[i - 1])
+            {
+                filteredText += text[i];
+            }
+        }
+
+        if (text[^1] != filteredText[^1])
+        {
+            filteredText += text[^1];
+        }
+
+        Console.WriteLine(filteredText);
+    }
+}
