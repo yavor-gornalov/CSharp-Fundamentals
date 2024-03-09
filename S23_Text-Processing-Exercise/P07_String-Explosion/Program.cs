755d721d9fe8b294b6e9aeb147c16320cfd78d6c
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-09 16:22:53 +0200

    String-Explosion --uploaded

---
+++ S23_Text-Processing-Exercise/P07_String-Explosion/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string text = Console.ReadLine();
+        string result = "";
+
+        int strength = 0;
+        int idx = 0;
+        while (idx < text.Length)
+        {
+            char prevElement = text[idx++];
+            if (prevElement == '>')
+            {
+                int startIdx = idx;
+                while (idx < text.Length && char.IsDigit(text[idx]))
+                {
+                    idx++;
+                    strength--;
+                }
+                strength += int.Parse(text.Substring(startIdx, idx - startIdx));
+                result += prevElement;
+            }
+            else
+            {
+                if (strength == 0)
+                {
+                    result += prevElement;
+                }
+                else
+                {
+                    strength--;
+                }
+            }
+        }
+
+        Console.WriteLine(result);
+    }
+}
