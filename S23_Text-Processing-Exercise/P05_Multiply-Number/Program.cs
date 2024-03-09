f0f2148456798e8a8221cd4e0ce270df0d0c7b3e
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-09 16:21:45 +0200

    P05_Multiply-Number --uploaded

---
+++ S23_Text-Processing-Exercise/P05_Multiply-Number/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Numerics;
+
+internal class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(
+            BigInteger.Parse(Console.ReadLine())
+            * BigInteger.Parse(Console.ReadLine()));
+    }
+}
