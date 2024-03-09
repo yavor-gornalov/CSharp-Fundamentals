bb3b832f80c38a9c6db7a2b8270191d9be158bc8
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-09 16:23:28 +0200

    Letters-and-Numbers --uploaded

---
+++ S23_Text-Processing-Exercise/P08_Letters-Numbers/Program.cs
@@ -0,0 +1,41 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+internal class Program
+{
+    static void Main()
+    {
+        string[] words = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .ToArray();
+
+        double totalSum = 0;
+
+        foreach (string word in words)
+        {
+            char first = word[0];
+            char last = word[^1];
+            double number = int.Parse(word.Substring(1, word.Length - 2));
+
+
+            if (char.IsUpper(first))
+            {
+                number /= first - 'A' + 1;
+            }
+            else
+            {
+                number *= first - 'a' + 1;
+            }
+
+            if (char.IsUpper(last))
+            {
+                number -= last - 'A' + 1;
+            }
+            else
+            {
+                number += last - 'a' + 1;
+            }
+            totalSum += number;
+        }
+        Console.WriteLine($"{totalSum:F2}");
+    }
+}
