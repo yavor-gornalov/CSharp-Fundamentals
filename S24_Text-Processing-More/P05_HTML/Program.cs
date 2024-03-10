5d02965273a17f8a4165ff915acd4f125699d372
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-10 17:49:58 +0200

    HTML --uploaded

---
+++ S24_Text-Processing-More/P05_HTML/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        string title = Console.ReadLine();
+        string article = Console.ReadLine();
+
+        StringBuilder output = new StringBuilder();
+        output.AppendLine($"<h1>\n\t{title}\n</h1>");
+        output.AppendLine($"<article>\n\t{article}\n</article>");
+
+        while (true)
+        {
+            string comment = Console.ReadLine();
+            if (comment == "end of comments")
+            {
+                break;
+            }
+            output.AppendLine($"<div>\n\t{comment}\n</div>");
+        }
+
+        Console.WriteLine(output.ToString());
+    }
+}
