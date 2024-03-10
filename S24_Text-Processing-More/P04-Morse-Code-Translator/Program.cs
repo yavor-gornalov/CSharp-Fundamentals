1fbd69c802aead31953a37e3199a64c6b56b7ae3
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-10 17:49:30 +0200

    Morse-Code-Translator --uploaded

---
+++ S24_Text-Processing-More/P04-Morse-Code-Translator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+internal class Program
+{
+    static void Main()
+    {
+        Dictionary<string, char> morseToAlpha = new Dictionary<string, char>()
+        {
+            {".-", 'A'}, {"-...", 'B'}, {"-.-.", 'C'}, {"-..", 'D'}, {".", 'E'},
+            {"..-.", 'F'}, {"--.", 'G'}, {"....", 'H'}, {"..", 'I'}, {".---", 'J'},
+            {"-.-", 'K'}, {".-..", 'L'}, {"--", 'M'}, {"-.", 'N'}, {"---", 'O'},
+            {".--.", 'P'}, {"--.-", 'Q'}, {".-.", 'R'}, {"...", 'S'}, {"-", 'T'},
+            {"..-", 'U'}, {"...-", 'V'}, {".--", 'W'}, {"-..-", 'X'}, {"-.--", 'Y'},
+            {"--..", 'Z'}
+        };
+
+        string[] words = Console.ReadLine().Split(" | ").ToArray();
+
+        StringBuilder decodedMessage = new StringBuilder();
+
+        foreach (var word in words)
+        {
+            foreach (var letter in word.Split(" ", StringSplitOptions.RemoveEmptyEntries))
+            {
+                decodedMessage.Append(morseToAlpha[letter]);
+            }
+            decodedMessage.Append(" ");
+        }
+        Console.WriteLine(decodedMessage.ToString());
+    }
+}
