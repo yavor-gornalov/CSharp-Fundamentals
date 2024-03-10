1f45a82fc6979bed6946362a5c531ca30af79de2
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-10 17:48:21 +0200

    Ascii-Sumator --uploaded

---
+++ S24_Text-Processing-More/P02_Ascii-Sumator/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        char first = char.Parse(Console.ReadLine());
+        char second = char.Parse(Console.ReadLine());
+        char[] line = Console.ReadLine().ToCharArray();
+
+        (char low, char high) = GetLimits(first, second);
+
+        char[] charRange = GetCharRange(low, high, line);
+
+        int totalSum = GetTotlaSum(charRange);
+        Console.WriteLine(totalSum);
+    }
+
+    private static int GetTotlaSum(char[] charRange)
+    {
+        return charRange.Sum(c => c);
+    }
+
+    private static char[] GetCharRange(char low, char high, char[] line)
+    {
+        return line.Where(x => x > low && x < high).ToArray();
+    }
+
+    private static (char low, char high) GetLimits(char first, char second)
+    {
+        char[] limits = new char[] { first, second }.OrderBy(x => x).ToArray();
+
+        return (limits[0], limits[1]);
+    }
+}
