209ddc1873ea1b311738b9ef37ef20aff784fac3
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-11 13:53:27 +0200

    Match-Phone-Number --uploaded

---
+++ S25_Regex/P02_Match-Phone-Number/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+internal class Program
+{
+    static void Main()
+    {
+        var pattern = @"\+359( |\-)2(\1)\d{3}(\1)\d{4}\b";
+        var numbers = Console.ReadLine();
+        var phoneMatches = Regex.Matches(numbers, pattern);
+
+        var phones = phoneMatches
+            .Cast<Match>()
+            .Select(p => p.Value.Trim())
+            .ToList();
+
+        Console.WriteLine(string.Join(", ", phones));
+    }
+}
