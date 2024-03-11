daec35d21f5eece97840c0f7ff4d20339b4210d2
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-11 13:53:01 +0200

    Match-Full-Name --uploaded

---
+++ S25_Regex/P01_Match-Full-Name/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+internal class Program
+{
+    static void Main()
+    {
+        string pattern = @"\b([A-Z][a-z]+ [A-Z][a-z]+)\b";
+        Regex re = new Regex(pattern, RegexOptions.Multiline);
+
+        string line = Console.ReadLine();
+
+        MatchCollection matches = re.Matches(line);
+
+        StringBuilder names = new StringBuilder();
+        foreach (Match match in matches)
+        {
+            names.Append(match.Groups[1] + " ");
+        }
+
+        Console.WriteLine(names.ToString());
+    }
+}
