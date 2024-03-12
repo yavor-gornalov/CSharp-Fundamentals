244d23665fa7cc7b499054fa1f64f33ee5b96a67
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-12 14:58:56 +0200

    Extract-Emails --uploaded

---
+++ S26_Regex-Exercise/P06_Extract-Emails/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string line = Console.ReadLine();
+
+        string emailPattern = @"\s(?<email>[a-zA-Z0-9]+([-._][a-zA-Z0-9]+)*@[a-zA-Z]+([-][a-zA-Z]+)*(\.[a-zA-Z]+([-][a-zA-Z]+)*)+)\b";
+        var re = new Regex(emailPattern);
+
+        MatchCollection matches = re.Matches(line);
+
+        foreach (Match match in matches)
+        {
+            Console.WriteLine(match.Groups["email"].Value);
+        }
+    }
+}
