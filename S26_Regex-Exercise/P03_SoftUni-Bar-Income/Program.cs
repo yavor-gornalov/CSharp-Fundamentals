ace5886617f0afee99771524e5b4df0c4cbc0bc3
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-12 14:57:30 +0200

    SoftUni-Bar-Income --uploaded

---
+++ S26_Regex-Exercise/P03_SoftUni-Bar-Income/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string pattern = @"^\%(?<customer>[A-Z][a-z]+)\%[^\%\|\$\.]*?\<(?<product>\w+)\>[^\%\|\$\.]*?\|(?<count>\d+)\|[^\%\|\$\.]*?(?<price>\d+(\.\d+)?)\$[^\%\|\$\.]*?$";
+        var re = new Regex(pattern);
+
+        double totalIncome = 0;
+        while (true)
+        {
+            string line = Console.ReadLine();
+
+            if (line == "end of shift")
+            {
+                Console.WriteLine($"Total income: {totalIncome:F2}");
+                break;
+            }
+
+            var match = re.Match(line);
+
+            if (match.Success)
+            {
+                string customer = match.Groups["customer"].Value;
+                string product = match.Groups["product"].Value;
+                int count = int.Parse(match.Groups["count"].Value);
+                double price = double.Parse(match.Groups["price"].Value);
+
+                double customerBill = price * count;
+                Console.WriteLine($"{customer}: {product} - {customerBill:F2}");
+                totalIncome += customerBill;
+            }
+        }
+    }
+}
