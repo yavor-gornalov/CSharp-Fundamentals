67a124024bc1837f25d7cd7709e206205f0235ef
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-13 21:40:10 +0200

    Santa-Secret-Helper --uploaded

---
+++ S27_Regex-More/P04_Santa-Secret-Helper/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+internal class Program
+{
+    static void Main()
+    {
+        var pattern = @"@(?<name>[A-Za-z]+)[^\@\-\!\:\>]*!(?<behavior>[G|N])!";
+        var re = new Regex(pattern);
+
+        var sb = new StringBuilder();
+
+        int key = int.Parse(Console.ReadLine());
+
+        while (true)
+        {
+            string line = Console.ReadLine();
+
+            if (line == "end") break;
+
+            string message = string.Join("", line.Select(x => (char)(x - key)).ToArray());
+
+            Match match = re.Match(message);
+
+            if (match.Success)
+            {
+                string name = match.Groups["name"].Value;
+                string behavior = match.Groups["behavior"].Value;
+
+                if (behavior == "G") sb.AppendLine(name);
+            }
+        }
+        Console.WriteLine(sb.ToString());
+    }
+}
