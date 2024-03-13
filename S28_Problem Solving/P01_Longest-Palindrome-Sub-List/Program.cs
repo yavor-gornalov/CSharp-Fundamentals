82eca4ffb12ef8e1617c250b9b8e2deae7ec8655
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-13 21:40:45 +0200

    Longest-Palindrome-Sub-List --uploaded

---
+++ S28_Problem Solving/P01_Longest-Palindrome-Sub-List/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        string letters = Console.ReadLine();
+        int maxLen = 0;
+        // Check all single letter central points
+        for (var c = 0; c < letters.Length; c++)
+            maxLen = Math.Max(maxLen, PalindromeLen(c, c));
+        // Check all double letter central points
+        for (var c = 0; c < letters.Length - 1; c++)
+            maxLen = Math.Max(maxLen, PalindromeLen(c, c + 1));
+        Console.WriteLine(maxLen);
+
+        int PalindromeLen(int leftIndex, int rightIndex)
+        {
+            while (leftIndex >= 0 && rightIndex < letters.Length
+                && letters[leftIndex] == letters[rightIndex])
+            {
+                leftIndex--;
+                rightIndex++;
+            }
+            return rightIndex - leftIndex - 1;
+        }
+    }
+}
