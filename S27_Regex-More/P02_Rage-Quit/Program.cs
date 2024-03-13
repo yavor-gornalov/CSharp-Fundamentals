fd5714faa3911a2c850f94d64156e91a441f1c8f
Yavor Gornalov <y.gornalov@gmail.com>
2024-03-13 21:37:55 +0200

    Rage-Quit --uploaded

---
+++ S27_Regex-More/P02_Rage-Quit/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+internal class Program
+{
+    static void Main()
+    {
+        string pattern = @"(?<string>([\D]+))(?<number>[\d]+)";
+        var re = new Regex(pattern);
+
+        string line = Console.ReadLine();
+
+        var result = new StringBuilder();
+
+        foreach (Match pair in re.Matches(line))
+        {
+            string symbols = pair.Groups["string"].Value;
+            int number;
+            if (!int.TryParse(pair.Groups["number"].Value, out number)) continue;
+
+
+            result.Append(string.Concat(Enumerable.Repeat(symbols.ToUpper(), number)));
+
+        }
+
+        int uniques = result.ToString().Distinct().ToArray().Length;
+        Console.WriteLine($"Unique symbols used: {uniques}");
+        Console.WriteLine(result.ToString());
+    }
+}
